685b58d6e5f3385ba8195a483fe487b654465193
Madusha Ravishani <mravishani7@gmail.com>
2021-10-21 12:14:45 +0530

    Delete RemovePatternsInputText.py

--- RemovePatternsInputText.py
+++
@@ -1,6 +0,0 @@
-# removes pattern in the input text
-def remove_pattern(input_txt, pattern):
-    r = re.findall(pattern, input_txt)
-    for word in r:
-        input_txt = re.sub(word, "", input_txt)
-    return input_txt
